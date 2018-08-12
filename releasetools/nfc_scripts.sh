2d5af76346321e8c6c68ad62b845fdf6bef85bcf
Jaiglissechef-i9100 <yaggiland@gmail.com>
2018-08-12 18:40:03 +0200

    Fix nfc

--- releasetools/nfc_scripts.sh
+++ releasetools/nfc_scripts.sh
@@ -23,9 +23,10 @@ getprop=`getprop ro.bootloader`
 variant=`echo $getprop | cut -c1-6`
 if [ $variant = "A320FL" ]
 then
-    rm /system/etc/libnfc-sec-hal.conf /system/etc/nfc/sec_s3nrn81_rfreg.bin /system/vendor/firmware/nfc/sec_s3nrn81_firmware.bin
+    rm /system/etc/libnfc-sec-hal.conf.s3nrn81 /system/etc/nfc/sec_s3nrn81_rfreg.bin /system/vendor/firmware/nfc/sec_s3nrn81_firmware.bin
 else
-    rm /system/etc/libnfc-sec-hal.conf.s3nrn80 /system/etc/sec_s3nrn80_rfreg.bin /system/vendor/firmware/sec_s3nrn80_firmware.bin
+    rm /system/etc/libnfc-sec-hal.conf /system/etc/sec_s3nrn80_rfreg.bin /system/vendor/firmware/sec_s3nrn80_firmware.bin
     mv /system/etc/libnfc-sec-hal.conf.s3nrn81 /system/etc/libnfc-sec-hal.conf
+fi
 fi
 exit 0
